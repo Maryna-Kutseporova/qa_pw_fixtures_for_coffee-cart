f82e20886b8e1b95a34d2ecd81bf16ed08e88ac6
Maryna-Kutseporova <Kutseperowa.marina@gmail.com>
2025-09-05 20:53:28 +0300

    fixtures, constants and helper  were added

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const COFFEE_PRICES = {
+  espresso: 10,
+  cappuccino: 19,
+  'espresso macchiato': 12,
+  mocha: 8,
+  'flat white': 18,
+  americano: 7,
+  'cafe latte': 16,
+  'espresso con panna': 14,
+  'cafe breve': 15,
+  'discounted mocha': 4,
+};
